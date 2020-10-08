54c15d2e2f038e97a94147a0d51887907de4c23c
codemasternode <marcinwarzybok@outlook.com>
2020-10-08 14:33:37 +0200

    Cover Movie Repository with tests

--- src/tests/unit/models/movie.spec.ts
+++ src/tests/unit/models/movie.spec.ts
@@ -1,6 +1,34 @@
 import { expect } from 'chai';
 import MovieRepository from '../../../models/movie/movie';
+import { MovieInDB } from '../../../types/movie';
 
 const movieRepository = new MovieRepository('../../../data/db-copy.json');
 
-describe('Movie Repository unit tests', () => {});
+describe('Movie Repository unit tests', () => {
+    describe('Should return created movie', () => {
+        it('when properly passed movie object', (done) => {
+            const movie: MovieInDB = movieRepository.addMovie({
+                title: 'Star Wars',
+                director: 'George Lucas',
+                genres: ['Sci-Fi'],
+                runtime: 127,
+                year: 1998,
+            });
+            done();
+        });
+    });
+    describe('Should throw error', () => {
+        it('made by MovieValidation validate method', (done) => {
+            expect(() => {
+                movieRepository.addMovie({
+                    title: 'Star Wars',
+                    director: 'George Lucas',
+                    genres: ['asddasasddsaads'],
+                    runtime: 127,
+                    year: 1998,
+                });
+            }).to.throw();
+            done();
+        });
+    });
+});
